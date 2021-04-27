2388b649dc3a1b0ed7244ef021d7494c6e5bff9d
priyanka nigam <priyanka.nigam_cs18@gla.ac.in>
2021-04-27 21:48:18 +0530

    connected with firebase

---
+++ login/src/fire.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+var firebaseConfig = {
+  apiKey: "AIzaSyCAairkwd0fmimrL98qawkSvpMVpI89CUU",
+  authDomain: "login-24f11.firebaseapp.com",
+  projectId: "login-24f11",
+  storageBucket: "login-24f11.appspot.com",
+  messagingSenderId: "344073194461",
+  appId: "1:344073194461:web:701fe327e1cef7be488434",
+};
+
+const fire = firebase.initializeApp(firebaseConfig);
+
+export default fire;
